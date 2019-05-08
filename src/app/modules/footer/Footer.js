9f81c69594f0cecb341e7dd085933c369f976a45
TrinhQuangvo <voquang1406@gmail.com>
2019-05-08 14:28:52 +0700

    footer

--- src/app/modules/footer/Footer.js
+++ src/app/modules/footer/Footer.js
@@ -1,9 +1,50 @@
 import React from 'react';
+import { Container, Row } from 'reactstrap'
+import Copyright from './Copyright'
+import FooterItems from './FooterItems'
+import FooterContact from './FooterContact'
+import Contact from './Contact'
+import BoxBrand from './BoxBrand'
+
 
 function Footer() {
+  const footerItems = [
+    {
+      heading: 'Thông Tin',
+      items: [
+        { id: 1, name: 'Về chúng tôi' }, { id: 2, name: 'Giao Hàng' },
+        { id: 3, name: 'Cảm Nghĩ' }, { id: 4, name: 'Lưu Trữ' },
+        { id: 5, name: 'Chính Sách Riêng Tư' }
+      ]
+    },
+    {
+      heading: 'Mua Hàng',
+      items: [
+        { id: 1, name: 'Về chúng tôi' }, { id: 2, name: 'Giao Hàng' },
+        { id: 3, name: 'Cảm Nghĩ' }, { id: 4, name: 'Lưu Trữ' },
+        { id: 5, name: 'Chính Sách Riêng Tư' }
+      ]
+    }
+  ]
+
+
+  var items = footerItems.map((item) => {
+    return <FooterItems heading={item.heading} items={item.items} path={item.path} />
+  })
   return (
-    <div>Footer</div>
+    <>
+    <BoxBrand/>
+    <footer className="footer">
+      <Container>
+        <Row>
+          {items}
+          <FooterContact />
+          <Contact />
+          <Copyright />
+        </Row>
+      </Container>
+    </footer>
+    </>
   )
 }
-
 export default Footer;
